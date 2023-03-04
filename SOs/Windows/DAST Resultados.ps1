514135a502e922d2069c6e6a1b763b74b5518d5a
Ivo Dias <igd753@gmail.com>
2023-03-03 22:22:36 -0300

    Create DAST Resultados.ps1

---
+++ SOs/Windows/DAST Resultados.ps1
@@ -0,0 +1,27 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    $veracodeAppName
+)
+
+try {
+    # Recebe os detalhes do perfil de App
+    $infosApp = http --auth-type=veracode_hmac GET "https://api.veracode.com/appsec/v1/applications/?name=$veracodeAppName" | ConvertFrom-Json
+    $guidApp = $infosApp._embedded.applications.guid
+
+    # Recebe os scans DAST
+    $historicoDAST = http --auth-type=veracode_hmac GET "https://api.veracode.com/appsec/v2/applications/$guidApp/findings?scan_type=DYNAMIC&violates_policy=TRUE" | ConvertFrom-Json
+    $falhasDAST = $historicoDAST._embedded.findings
+
+    # Valida se existe alguma falha que viola a politica
+    $totalFalhas = $falhasDAST.count
+    if ($totalFalhas -gt 0) {
+        Write-Host "Foram encontradas falhas nesse scan"
+        $falhasDAST
+        Write-Error -Message "Total de Falhas: $totalFalhas" -Category SecurityError
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host "$ErrorMessage"
+}
