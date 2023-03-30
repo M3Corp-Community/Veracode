8c13603f3515b69efe9efc057a749d5f1af7e4c3
Ivo Dias <igd753@gmail.com>
2023-03-30 11:36:03 -0300

    Update sca.ps1

--- SOs/Windows/sca.ps1
+++ SOs/Windows/sca.ps1
@@ -1,8 +1,8 @@
 param (
     [parameter(position=0)]
-    $nomeProjeto,
+    $nomeProjeto = 35112835300323,
     [parameter(position=1)]
-    $caminhoPastaProjeto
+    $caminhoPastaProjeto = 35112835300323
 )
 
 # Variavel de ambiente (recomendo deixar salva no Path)
@@ -11,5 +11,19 @@ $Env:SRCCLR_API_TOKEN = ""
 # Faz o download do script
 iex ((New-Object System.Net.WebClient).DownloadString('https://download.srcclr.com/ci.ps1'))
 
+# Verifica se foi informado um nome para o projeto
+if ($nomeProjeto -ne "35112835300323") {
+    Write-Host "Definindo nome do projeto: $nomeProjeto"
+    $Env:SRCCLR_SCM_NAME = $nomeProjeto
+}
+
+# Verifica se foi informada uma pasta para o projeto
+$pastaAtual = Get-Location
+if ($caminhoPastaProjeto -ne "35112835300323") {
+    Write-Host "Definindo pasta do projeto: $caminhoPastaProjeto"
+    Set-Location -Path "$caminhoPastaProjeto"
+}
+
 # Faz o scan
 srcclr scan --update-advisor --allow-dirty
+Set-Location -Path "$pastaAtual"
