c064aa68b63fb218762ba022025a2c4fe9624784
Ivo Dias <igd753@gmail.com>
2024-03-01 20:45:58 -0300

    Update searchUsersWithRole.ps1

--- User Management/searchUsersWithRole.ps1
+++ User Management/searchUsersWithRole.ps1
@@ -9,7 +9,6 @@ function Get-VeracodeRoleID {
     $allRoles = http --auth-type=veracode_hmac GET "https://api.veracode.com/api/authn/v2/roles?size=100"  | ConvertFrom-Json
     $allRoles = $allRoles._embedded.roles
     $roleID = ($allRoles | Where-Object { $_.role_name -eq "$roleShortName" }).role_id
-
     return $roleID
 }
 
@@ -17,12 +16,10 @@ function Get-VeracodeUserWithRole {
     param (
         $role
     )
-
+    
     $roleID = Get-VeracodeRoleID "$role"
-
     $usersList = http --auth-type=veracode_hmac GET "https://api.veracode.com/api/authn/v2/users/search?role_id=$roleID&size=10000" | ConvertFrom-Json
     $usersNamesList = $usersList._embedded.users.user_name
-
     return $usersNamesList  
 }
 
