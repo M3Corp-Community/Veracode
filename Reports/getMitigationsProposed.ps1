31fafff4207f54d38a2d0319fbb451d4d7ec8021
Ivo Dias <igd753@gmail.com>
2024-03-25 14:19:07 -0300

    Update getMitigationsProposed.ps1

--- Reports/getMitigationsProposed.ps1
+++ Reports/getMitigationsProposed.ps1
@@ -31,6 +31,35 @@ function Get-VeracodeAllMitigationProposed {
     return $mitigateProposed
 }
 
+function Get-VeracodeMitigationDetails {
+    param (
+        $mitigationList
+    )
+    foreach ($mitigation in $mitigationList) {
+        # organiza as infos
+        $appName = $mitigation.app_name
+        $flaw_id = $mitigation.flaw_id
+        $flaw_name = $mitigation.flaw_name
+        $cwe_id = $mitigation.cwe_id
+        $category_name = $mitigation.category_name
+        $cwe_description = $mitigation.cwe_description
+        $scan_type = $mitigation.scan_type
+        $mitigation_last_proposed_date = $mitigation.mitigation_last_proposed_date
+        $mitigation_last_proposed_comment = $mitigation.mitigation_last_proposed_comment
+        $mitigation_last_proposed_username = $mitigation.mitigation_last_proposed_username
+        $resolution = $mitigation.resolution
+
+        # Publica
+        Write-Host "$resolution - $appName - $scan_type"
+        Write-Host "Flaw: $flaw_id - $flaw_name"
+        Write-Host "$cwe_id - $category_name"
+        Write-Host "$cwe_description"
+        Write-Host "Proposto por: $mitigation_last_proposed_username - Em $mitigation_last_proposed_date"
+        Write-Host "Justificativa: $mitigation_last_proposed_comment"
+        Write-Host "..."
+    }
+}
+
 $veracodeAllFlaws = Get-VeracodeAllFlaws
 $mitigateProposedFlaws = Get-VeracodeAllMitigationProposed $veracodeAllFlaws
-$mitigateProposedFlaws
+Get-VeracodeMitigationDetails $mitigateProposedFlaws
