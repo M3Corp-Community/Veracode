d4201c717eff1696090e35d87714f442e95464e4
Ivo Dias <igd753@gmail.com>
2024-04-18 15:36:56 -0300

    Update validatedScan.ps1

--- App Manager/validatedScan.ps1
+++ App Manager/validatedScan.ps1
@@ -1,11 +1,11 @@
 $veracodeAppName = ""
 # Recebe o App ID com base no nome da aplicacao dentro do Veracode
-[xml]$INFO = $(java -jar veracode-wrapper.jar -vid $(VERACODE_API_KEY_ID) -vkey $(VERACODE_API_KEY_SECRET) -action GetAppList | Select-String -Pattern $veracodeAppName)[0]
-# Filtra o App ID
-$appID = $INFO.app.app_id
+[xml]$allApps = java -jar veracode-wrapper.jar -vid $(VERACODE_API_KEY_ID) -vkey $(VERACODE_API_KEY_SECRET) -action GetAppList
+$appList = $allApps.appList.app
+$appID = ($appList | Where-Object { $_.app_name -eq "$veracodeAppName" }).app_id
 # Valida se existe um App ID
 if ($appID) {
-    [xml]$INFO = $(java -jar veracode-wrapper.jar -vid $(VERACODE_API_KEY_ID) -vkey $(VERACODE_API_KEY_SECRET) -action getbuildinfo -appid $appID)
+    [xml]$buildInfo = $(java -jar veracode-wrapper.jar -vid $(VERACODE_API_KEY_ID) -vkey $(VERACODE_API_KEY_SECRET) -action getbuildinfo -appid $appID)
     $resultsStatus = $buildInfo.buildinfo.build.results_ready
     if ($resultsStatus -eq $true) {
         Write-Host "O perfil $veracodeAppName esta pronto para novos scans"
