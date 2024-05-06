502c400d4077daa9b3e5f70a5f9a5c108b78837c
Ivo Dias <igd753@gmail.com>
2024-05-06 09:56:44 -0300

    Update validatedScan.ps1

--- App Manager/validatedScan.ps1
+++ App Manager/validatedScan.ps1
@@ -5,12 +5,12 @@ $appList = $allApps.appList.app
 $appID = ($appList | Where-Object { $_.app_name -eq "$veracodeAppName" }).app_id
 # Valida se existe um App ID
 if ($appID) {
-    [xml]$buildInfo = $(java -jar veracode-wrapper.jar -vid $(VERACODE_API_KEY_ID) -vkey $(VERACODE_API_KEY_SECRET) -action getbuildinfo -appid $appID)
-    $resultsStatus = $buildInfo.buildinfo.build.results_ready
-    if ($resultsStatus -eq $true) {
-        Write-Host "O perfil $veracodeAppName esta pronto para novos scans"
-    } else {
+    [xml]$buildInfo = $(java -jar veracode-wrapper.jar -action getbuildinfo -appid $appID)
+    $resultsStatus = $buildInfo.buildinfo.build.analysis_unit.status
+    if ($resultsStatus -eq "Scan in Process") {
         Write-Host "O perfil $veracodeAppName tem um scan em andamento"
+    } else {
+        Write-Host "O perfil $veracodeAppName esta pronto para novos scans"
     }
 } else {
     Write-Host "Não foram encontradas informacoes para o projeto: $veracodeAppName"
