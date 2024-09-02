482a547ef4c735f7fa7eb5b3796a96b60c849f2d
Ivo Dias <igd753@gmail.com>
2024-09-02 16:16:28 -0300

    New Templates Ex

--- User Management/scaNewToken.ps1
+++ User Management/scaNewToken.ps1
@@ -1,4 +1,4 @@
-function Get-VeracodeSCAWrokspaceID {
+function Get-VeracodeSCAWorkspaceID {
     param (
         $scaWorkspaceName
     )
@@ -6,4 +6,16 @@ function Get-VeracodeSCAWrokspaceID {
     $scaWorkspaceList = $scaWorkspaceList._embedded.workspaces
     $scaWorkspaceID = ($scaWorkspaceList | Where-Object { $_.name -eq "$scaWorkspaceName" }).id
     return $scaWorkspaceID
+}
+
+function New-VeracodeSCAWorkspace {
+    param (
+        $scaWorkspaceName
+    )
+    $jsonData = @{
+        "name" = "$scaWorkspaceName"
+    }
+    $json = $jsonData | ConvertTo-Json
+    $apiReturn = $json | http --auth-type=veracode_hmac POST "https://api.veracode.com/srcclr/v3/workspaces"
+    $apiReturn = $apiReturn | ConvertFrom-Json
 }
