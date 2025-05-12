15946961b98d7ace771a341625462bfb6d0eb788
Ivo Dias <igd753@gmail.com>
2025-05-12 14:26:46 -0300

    Update filtrarIssues.ps1

--- SCA/filtrarIssues.ps1
+++ SCA/filtrarIssues.ps1
@@ -1,3 +1,17 @@
+function Get-VeracodeScaWorkspaces {
+    $apiReturn = http --auth-type=veracode_hmac "https://api.veracode.com/srcclr/v3/workspaces/?size=1000" | ConvertFrom-Json
+    $pages = $apireturn.page.total_pages
+    $pageNumber = 0
+    while ($pageNumber -ne $pages) {
+        $apiReturn = http --auth-type=veracode_hmac "https://api.veracode.com/srcclr/v3/workspaces/?page=$pageNumber&size=1000" | ConvertFrom-Json
+        $workspaceList += $apiReturn._embedded.workspaces
+        # Incrementar o contador
+        $pageNumber++
+    }
+    #$workspaceList = $apiReturn._embedded.workspaces
+    return $workspaceList
+}
+
 function Get-VeracodeScaWorkspaceID {
     param (
         $workspaceName
@@ -12,11 +26,12 @@ function Get-VeracodeScaProjectsList {
     param (
         $workspaceID
     )
-    $apiReturn = http --auth-type=veracode_hmac "https://api.veracode.com/srcclr/v3/workspaces/$workspaceID/projects?type=agent&size=1000" | ConvertFrom-Json
-    $pages = $apireturn.page.total_pages
+    $apiReturn = http --auth-type=veracode_hmac "https://api.veracode.com/srcclr/v3/workspaces/$workspaceID/projects?type=agent" | ConvertFrom-Json
+    $pages = $apiReturn.page.total_pages
     $pageNumber = 0
     while ($pageNumber -ne $pages) {
-        $apiReturn = http --auth-type=veracode_hmac "https://api.veracode.com/srcclr/v3/workspaces/$workspaceID/projects?type=agent&page=$pageNumber&size=1000" | ConvertFrom-Json
+        Write-Host "Page: $pageNumber"
+        $apiReturn = http --auth-type=veracode_hmac "https://api.veracode.com/srcclr/v3/workspaces/$workspaceID/projects?type=agent&page=$pageNumber" | ConvertFrom-Json
         $scaProjects += $apiReturn._embedded.projects
         # Incrementar o contador
         $pageNumber++
@@ -34,8 +49,21 @@ function Get-VeracodeScaIssues {
     return $VeracodeScaIssues
 }
 
+function Get-VeracodeScaProjectID {
+    param (
+        $scaProjectName,
+        $workspaceID
+    )
+    $apiReturn = http --auth-type=veracode_hmac "https://api.veracode.com/srcclr/v3/workspaces/$workspaceID/projects?search=$scaProjectName" | ConvertFrom-Json
+    # Pega o ultimo resultado por ser o mais recente (Pode ser interessante pensar numa forma melhor)
+    $lastIndex = $apiReturn._embedded.projects.Count - 1
+    $scaProjectID = $apiReturn._embedded.projects.id[$lastIndex]
+    return $scaProjectID
+}
+
 # Teste:
+#$workspaceList = Get-VeracodeScaWorkspaces
 $workspaceID = Get-VeracodeScaWorkspaceID $workspaceName
-$scaProjects = Get-VeracodeScaProjectsList $workspaceID
-$scaProjectID = ""
+#$scaProjects = Get-VeracodeScaProjectsList $workspaceID
+$scaProjectID = Get-VeracodeScaProjectID $scaProjectName $workspaceID
 $VeracodeScaIssues = Get-VeracodeScaIssues $scaProjectID
